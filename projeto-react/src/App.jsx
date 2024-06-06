8bdf3b457921acf0639cf030af41d4b8cc639619
johnicassere <johnicassere@hotmail.com>
2024-06-06 12:08:32 -0300

    finalizando mod

--- projeto-react/src/App.jsx
+++ projeto-react/src/App.jsx
@@ -1,22 +1,44 @@
 import { useState } from "react";
 import Perfil from "./componentes/Perfil";
 import Formulario from "./componentes/Formulario";
-import Repo from "./componentes/Repositorio";
+import Repo from "./componentes/ReposList";
+import './global.css'
 
 function App() {
-const [formVisivel, setFormVisivel] = useState(true)
- 
+  //const [formVisivel, setFormVisivel] = useState(true);
+  const [nomeUsuario, setNomeUsuario] = useState('');
 
-return (
-  <>
-  <Perfil nome="Johni" endereco="https://github.com/johnicassere.png"/>
-    {/* {formVisivel && (
+
+  return (
+    <>
+      <div className="input_home">
+        {
+          !nomeUsuario && (
+            <>
+            <label>Digite o nome do usuario do Git Hub</label>
+            <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} placeholder="Digite o usuario Git hub" />
+            
+            </>
+          )
+        }
+        {
+          nomeUsuario.length > 4 && (
+            <>
+              <Perfil nomeUsuario={nomeUsuario} />
+              <Repo nomeUsuario={nomeUsuario} />
+            </>
+
+          )
+        }
+
+
+        {/* {formVisivel && (
     <Formulario/>
   )}
   <button type="button" onClick={() => setFormVisivel(!formVisivel)}>toggle</button> */}
-  <Repo/>
-  </>
-)
+      </div>
+    </>
+  )
 }
 
 export default App
